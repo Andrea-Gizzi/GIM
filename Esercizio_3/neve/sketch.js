0f6af6959d70c506d4059fbf26aa63583996784b
Andrea-Gizzi <andreagizzi2003@gmail.com>
2023-05-23 15:30:27 +0200

    3 neve

--- Esercizio_3/neve/sketch.js
+++ Esercizio_3/neve/sketch.js
@@ -1,5 +1,6 @@
 
-let snowflakes = []; // array to hold snowflake objects
+let snowflakes = [];
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,49 +8,53 @@ function setup() {
   noStroke();
 }
 
+
+
 function draw() {
   background(0);
-  let t = frameCount / 50; // update time
+  let t = frameCount / 50;
 
-  // create a random number of snowflakes each frame
-  for (let i = 0; i < random(5); i++) {
-    snowflakes.push(new snowflake()); // append snowflake object
+
+  for (let i = 0; i < random(6); i++) {
+    snowflakes.push(new snowflake());
   }
 
-  // loop through snowflakes with a for..of loop
+
+
   for (let flake of snowflakes) {
-    flake.update(t); // update snowflake position
-    flake.display(); // draw snowflake
+    flake.update(t);
+    flake.display();
   }
 }
 
-// snowflake class
+
+
 function snowflake() {
-  // initialize coordinates
   this.posX = 0;
   this.posY = random(-50, 0);
   this.initialangle = random(0, 6 * PI);
   this.size = random(0,5, 7);
 
-  // radius of snowflake spiral
-  // chosen so the snowflakes are uniformly spread out in area
+  
   this.radius = sqrt(random(pow(width / 2, 2)));
 
+
   this.update = function(time) {
-    // x position follows a circle
-    let w = 0.4; // angular speed
+    let w = 0.3;
     let angle = w * time + this.initialangle;
     this.posX = width / 2 + this.radius * sin(angle);
 
-    // different size snowflakes fall at slightly different y speeds
+
     this.posY += pow(this.size, 0.5);
 
-    // delete snowflake if past end of screen
+
     if (this.posY > height) {
       let index = snowflakes.indexOf(this);
       snowflakes.splice(index, 1);
     }
   };
+
+
 
   this.display = function() {
     ellipse(this.posX, this.posY, this.size);
