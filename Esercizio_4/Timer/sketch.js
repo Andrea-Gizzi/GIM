840cbecdccaf6152acf3d18a3ea9fa5404685ff6
Andrea-Gizzi <andreagizzi2003@gmail.com>
2023-06-10 16:44:16 +0200

    Timer

--- Esercizio_4/Timer/sketch.js
+++ Esercizio_4/Timer/sketch.js
@@ -1,56 +1,69 @@
+let filterActive = false;
+let W = 255
 
-function setup(){
-	createCanvas(windowWidth, windowHeight)
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 }
 
-function draw(){
-	let h = hour();
-	let m = minute();
-	let s = second();
+function draw() {
+  let h = hour();
+  let m = minute();
+  let s = second();
 
-	//sfondo
-	const r = ((h/24)*255)
-	const g = ((m/60)*255)
-	const b = ((s/60)*255)
-	background (r, g, b)
+  //sfondo
+  const r = ((h / 24) * 255);
+  const g = ((m / 60) * 255);
+  const b = ((s / 60) * 255);
+  background(r, g, b);
 
-    //timer
-	const d = ((h/24)*245+10)
-	const d1 = ((m/60)*245+10)
-	const d2 = ((s/60)*245+10)
+  //timer
+  const d = ((h / 24) * 245 + 10);
+  const d1 = ((m / 60) * 245 + 10);
+  const d2 = ((s / 60) * 245 + 10);
+   
+  //filtro
+  if (filterActive) {
+    filter(INVERT);
+  }
 
-	//:
-	fill(255, 255, 255, 200)
-	text(":     :",width/4 * 2, height/2)
-	
-	if(h < 10) h = "0" + h
-	if(m < 10) m = "0" + m
-	if(s < 10) s = "0" + s
-	textSize(windowWidth/5)
-	textFont('times')
-	textAlign(CENTER, CENTER)
+  //:
+  fill(W, W, W, 200)
+  text(":     :", width / 4 * 2, height / 2);
 
-    //hour
-	fill(255, 255, 255, d)
-	text(h, width/5 * 1, height/2)
+  if (h < 10) h = "0" + h;
+  if (m < 10) m = "0" + m;
+  if (s < 10) s = "0" + s;
+  textSize(windowWidth / 5);
+  textFont('times');
+  textAlign(CENTER, CENTER);
 
-	//min
-	fill(255, 255, 255, d1)
-	text(m, width/4 * 2, height/2)
-	
-	//sec
-	fill(255, 255, 255, d2)
-	text(s, width/5 * 4, height/2)
+  //hour
+  fill(W, W, W, d);
+  text(h, width / 5 * 1, height / 2);
+
+  //min
+  fill(W, W, W, d1);
+  text(m, width / 4 * 2, height / 2);
+
+  //sec
+  fill(W, W, W, d2);
+  text(s, width / 5 * 4, height / 2);
+
+  if (h > 18 || h < 6) W = 0
+
 }
 
-function windowResized(){
-	resizeCanvas(windowWidth, windowHeight)
-	background(0)
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
-function keyPressed(){
-	if (key === ' ') {
-		filter(INVERT);
-	}
+function keyPressed() {
+  if (key === ' ') {
+    filterActive = !filterActive;
+  }
 }
-// realizzato trammite l'esempio svolto in atelier e succesivamente svilluppato
+
+function mousePressed() {
+	text(0)
+}
